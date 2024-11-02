f154f60fa87e067ee56d9a9ec4cf4a41ead4c8ec
Lukáš Gorazd Hrodek <lukas.gorazd.hrodek@gmail.com>
2024-11-02 20:48:17 +0100

    feat: add sidebar

--- hc-next/tailwind.config.ts
+++ hc-next/tailwind.config.ts
@@ -10,20 +10,31 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      colors: {},
-    },
+  	extend: {
+  		fontFamily: {
+  			sans: ["var(--font-geist-sans)", ...fontFamily.sans]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
+  		}
+  	}
   },
 
-  darkMode: "class",
+  darkMode: ["class", "class"],
   plugins: [
     nextui({
       themes: {
