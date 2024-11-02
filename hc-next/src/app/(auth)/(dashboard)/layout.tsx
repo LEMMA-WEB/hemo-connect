8074d521a8a3e2b7b02e4a13a69086ed793adf3d
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2024-11-02 22:20:06 +0100

    feat: Add resiable panel to dashboard

--- hc-next/src/app/(auth)/(dashboard)/layout.tsx
+++ hc-next/src/app/(auth)/(dashboard)/layout.tsx
@@ -2,6 +2,29 @@
 
 import { type PropsWithChildren } from "react";
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
 export default function DashboardLayout({ children }: PropsWithChildren) {
-  return children;
+  return (
+    <div className="flex h-screen w-full flex-col overflow-hidden">
+      <div className="border-b-1 border-neutral-200">HEADER</div>
+      <div className="flex-1">
+        <ResizablePanelGroup direction="horizontal">
+          <ResizablePanel defaultSize={20} minSize={20} maxSize={30}>
+            MENU SIDEBAR
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={40}>{children}</ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel max-size={40} defaultSize={0}>
+            REFERENCE SIDEBAR
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
+    </div>
+  );
 }
