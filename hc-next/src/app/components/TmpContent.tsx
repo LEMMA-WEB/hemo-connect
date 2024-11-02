879ec22d9d0a4e20e6bc0b6cbd56c181d9583d8c
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2024-11-02 16:37:01 +0100

    feat: Add nextui and import sorting

--- hc-next/src/app/components/TmpContent.tsx
+++ hc-next/src/app/components/TmpContent.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 export function TmpContent() {
   const { data: session } = useSession();
