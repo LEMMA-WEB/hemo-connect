163074ee6d9b8543fdfc8916aef83408eb1f8966
Lukáš Gorazd Hrodek <lukas.gorazd.hrodek@gmail.com>
2024-11-03 01:30:16 +0100

    feat: add react icons and remove color

--- hc-next/src/app/(auth)/(dashboard)/diagnosis/page.tsx
+++ hc-next/src/app/(auth)/(dashboard)/diagnosis/page.tsx
@@ -7,7 +7,7 @@ export default function Diagnosis() {
         <h1 className="text-4xl font-bold">Jméno Příjmení</h1>
         <div className="flex justify-around px-4 pt-2">
           <p className="text-lg text-neutral-500">1. 1. 1999</p>
-          <div className="border-3 min-h-48 min-w-24 rounded-md border-black bg-red-700 p-8 text-white">
+          <div className="border-3 min-h-48 min-w-24 rounded-md border-black bg-neutral-400 p-8">
             <h3 className="pb-2 text-xl font-medium">Výsledek Laboratoře</h3>
             <p className="text-sm">Hemofilie A</p>
           </div>
