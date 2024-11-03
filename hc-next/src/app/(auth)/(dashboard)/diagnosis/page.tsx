a3c6b141401592966e9bc4cd4bf26f03f2b85263
Lukáš Gorazd Hrodek <lukas.gorazd.hrodek@gmail.com>
2024-11-03 04:00:12 +0100

    fix: Chat

--- hc-next/src/app/(auth)/(dashboard)/diagnosis/page.tsx
+++ hc-next/src/app/(auth)/(dashboard)/diagnosis/page.tsx
@@ -1,16 +1,7 @@
-import Table from "@/components/Table";
+"use client";
+
+import Chat from "@/components/Chat";
 
 export default function Diagnosis() {
-  return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold">Jméno Příjmení</h1>
-      <div className="flex justify-around px-4 pt-2">
-        {/* <div className="border-3 min-h-48 min-w-24 rounded-md border-black bg-neutral-400 p-8">
-            <h3 className="pb-2 text-xl font-medium">Výsledek Laboratoře</h3>
-            <p className="text-sm">Hemofilie A</p>
-          </div> */}
-      </div>
-      <Table />
-    </div>
-  );
+  return <Chat />;
 }
