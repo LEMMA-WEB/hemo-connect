6a7b6f3684e5aad377913504ee0161f6b9c1a9bc
Lukáš Gorazd Hrodek <lukas.gorazd.hrodek@gmail.com>
2024-11-03 04:59:13 +0100

    fix: Table refactor for props

--- hc-next/src/app/(auth)/diagnosis/[diagnosisId]/patients/page.tsx
+++ hc-next/src/app/(auth)/diagnosis/[diagnosisId]/patients/page.tsx
@@ -1,10 +1,32 @@
 import Table from "@/components/Table";
+import {
+  columns,
+  records as data,
+  descOptions as pickOptions,
+} from "@/data/data";
 
 export default function Diagnosis() {
   return (
     <div className="p-8 px-12">
       <h1 className="text-4xl font-bold">DATA z NISu</h1>
-      <Table />
+      <Table
+        {...{
+          columns,
+          data,
+          pickOptions,
+          initialVisibleColumns: [
+            "ic_amb_zad",
+            "dat_zad",
+            "cas_zad",
+            "prac_od",
+            "text_dg",
+            "actions",
+          ],
+          pickFilterField: "text_dg",
+          idColumn: "ic_amb_zad",
+          searchField: "amb_zaz_text",
+        }}
+      />
     </div>
   );
 }
